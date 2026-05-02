ac444bf261432c818af8c7adf4065f25de77a591
seonghwa yu <moristyu50@gmail.com>
2026-05-02 10:34:33 +0900

    fix: include firebase_config.js for live site deployment

---
+++ js/firebase_config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCr4pQC9kAS5QG0s3VDRdEYDqd08uducSY",
+  authDomain: "schoolmanage87.firebaseapp.com",
+  databaseURL: "https://schoolmanage87-default-rtdb.firebaseio.com",
+  projectId: "schoolmanage87",
+  storageBucket: "schoolmanage87.firebasestorage.app",
+  messagingSenderId: "994976889761",
+  appId: "1:994976889761:web:23e06a6fefc3af95bc69e0",
+  measurementId: "G-48F460MVRZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
